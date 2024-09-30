023faadd357d8325152fa3c4b009d8f5a6f6cf96
test <test@test.com>
2024-09-30 19:50:45 +0900

    2강 문제 풀이

--- src/main/java/com/quiz/lesson02/Lesson02Quiz01RestController.java
+++ src/main/java/com/quiz/lesson02/Lesson02Quiz01RestController.java
@@ -1,9 +1,23 @@
 package com.quiz.lesson02;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class Lesson02Quiz01RestController {
+import com.quiz.lesson02.bo.StoreBO;
+import com.quiz.lesson02.domain.Store;
 
+@RestController // @Controller + @ResponseBody
+public class Lesson02Quiz01RestController {
 	
+	@Autowired // DI
+	private StoreBO storeBO; 
+
+	// http://localhost:8080/lesson02/quiz01
+	@RequestMapping("/lesson02/quiz01")
+	public List<Store> quiz01() {
+		return storeBO.getStoreList(); // list json => response
+	}
 }
